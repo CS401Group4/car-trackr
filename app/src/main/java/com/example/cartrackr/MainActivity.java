ac843f1b3f0ba376c5d9c38dc79791c610d961f3
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-04-13 21:48:15 -0700

    Add Google sign in feature

--- app/src/main/java/com/example/cartrackr/MainActivity.java
+++ app/src/main/java/com/example/cartrackr/MainActivity.java
@@ -6,15 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+
+    private GoogleSignInClient mSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth and check if the user is signed in
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        if (mFirebaseAuth.getCurrentUser() == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        }
+
+        Intent intent = new Intent(this, AddCarPage.class);
+        startActivity(intent);
     }
 
     public void launchActivity(View view) {
         Intent intent = new Intent(this, AddCarPage.class);
         startActivity(intent);
+    }
+
+    private void signOut() {
+        mFirebaseAuth.signOut();
+        mSignInClient.signOut();
+        startActivity(new Intent(this, SignInActivity.class));
+        finish();
     }
 }
