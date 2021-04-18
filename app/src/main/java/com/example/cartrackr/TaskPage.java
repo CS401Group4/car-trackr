b4931bf59157786a4a7589083ec1f1bccb02e36c
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-04-18 12:05:20 -0700

    Fix add car page design

--- app/src/main/java/com/example/cartrackr/TaskPage.java
+++ app/src/main/java/com/example/cartrackr/TaskPage.java
@@ -2,13 +2,46 @@ package com.example.cartrackr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TaskPage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void signOut() {
+        AddCarPage.mFirebaseAuth.signOut();
+        AddCarPage.mSignInClient.signOut();
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
