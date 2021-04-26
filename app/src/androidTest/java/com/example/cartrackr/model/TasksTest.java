1755d4ef0749373d05d051ec371fa097385c1586
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-04-25 17:53:47 -0700

    Generate javadoc

--- app/src/androidTest/java/com/example/cartrackr/model/TasksTest.java
+++ app/src/androidTest/java/com/example/cartrackr/model/TasksTest.java
@@ -4,21 +4,38 @@ import com.google.android.gms.tasks.Task;
 
 import junit.framework.TestCase;
 
+/**
+ * Unit tests for Tasks Model
+ * @since April 2021
+ * @author Group 4
+ */
 public class TasksTest extends TestCase {
 
+    /**
+     * Test for toString override method
+     */
     public void testTestToString() {
     }
 
+    /**
+     * Test for service name getter
+     */
     public void testGetServiceName() {
         Tasks tasks = new Tasks("Change oil", "5/1/2021");
         assertEquals(tasks.getServiceName(), "Change oil");
     }
 
+    /**
+     * Second Test for service name getter
+     */
     public void testGetServiceName2() {
         Tasks tasks = new Tasks("Car wash", "5/1/2021");
         assertEquals(tasks.getServiceName(), "Car wash");
     }
 
+    /**
+     * Test for service name setter
+     */
     public void testSetServiceName() {
         Tasks tasks = new Tasks();
         String taskName = "Tire change";
@@ -26,6 +43,9 @@ public class TasksTest extends TestCase {
         assertEquals(tasks.getServiceName(), taskName);
     }
 
+    /**
+     * Second Test for service name setter
+     */
     public void testSetServiceName2() {
         Tasks tasks = new Tasks();
         String taskName = "Change oil";
@@ -33,16 +53,25 @@ public class TasksTest extends TestCase {
         assertEquals(tasks.getServiceName(), taskName);
     }
 
+    /**
+     * Test for service date getter
+     */
     public void testGetServiceDate() {
         Tasks tasks = new Tasks("Car wash", "5/1/2021");
         assertEquals(tasks.getServiceDate(), "5/1/2021");
     }
 
+    /**
+     * Second Test for service date getter
+     */
     public void testGetServiceDate2() {
         Tasks tasks = new Tasks("Change oil", "6/1/2021");
         assertEquals(tasks.getServiceDate(), "6/1/2021");
     }
 
+    /**
+     * Test for service date setter
+     */
     public void testSetServiceDate() {
         Tasks tasks = new Tasks();
         String taskDate = "5/5/2021";
@@ -50,6 +79,9 @@ public class TasksTest extends TestCase {
         assertEquals(tasks.getServiceDate(), taskDate);
     }
 
+    /**
+     * Second Test for service date setter
+     */
     public void testSetServiceDate2() {
         Tasks tasks = new Tasks();
         String taskDate = "6/1/2023";
--- app/src/androidTest/java/com/example/cartrackr/model/VehicleTest.java
+++ app/src/androidTest/java/com/example/cartrackr/model/VehicleTest.java
@@ -2,16 +2,30 @@ package com.example.cartrackr.model;
 
 import junit.framework.TestCase;
 
+/**
+ * Unit Test for Vehicle model
+ * @since April 2021
+ * @author Group 4
+ */
 public class VehicleTest extends TestCase {
 
+    /**
+     * Test for toString override method
+     */
     public void testTestToString() {
     }
 
+    /**
+     * Test for id getter
+     */
     public void testGetId() {
         Vehicle vehicle = new Vehicle("1", "Toyota", "Corolla", 2021, "sampleToken");
         assertEquals(vehicle.getId(), "1");
     }
 
+    /**
+     * Test for id setter
+     */
     public void testSetId() {
         Vehicle vehicle = new Vehicle();
         String id = "1";
@@ -19,11 +33,17 @@ public class VehicleTest extends TestCase {
         assertEquals(vehicle.getId(), id);
     }
 
+    /**
+     * Test for make getter
+     */
     public void testGetMake() {
         Vehicle vehicle = new Vehicle("1", "Toyota", "Corolla", 2021, "sampleToken");
         assertEquals(vehicle.getMake(), "Toyota");
     }
 
+    /**
+     * Test for make setter
+     */
     public void testSetMake() {
         Vehicle vehicle = new Vehicle();
         String make = "Toyota";
@@ -31,11 +51,17 @@ public class VehicleTest extends TestCase {
         assertEquals(vehicle.getMake(), make);
     }
 
+    /**
+     * Test for model getter
+     */
     public void testGetModel() {
         Vehicle vehicle = new Vehicle("1", "Toyota", "Corolla", 2021, "sampleToken");
         assertEquals(vehicle.getModel(), "Corolla");
     }
 
+    /**
+     * Test for model setter
+     */
     public void testSetModel() {
         Vehicle vehicle = new Vehicle();
         String model = "Corolla";
@@ -43,11 +69,17 @@ public class VehicleTest extends TestCase {
         assertEquals(vehicle.getModel(), model);
     }
 
+    /**
+     * Test for year getter
+     */
     public void testGetYear() {
         Vehicle vehicle = new Vehicle("1", "Toyota", "Corolla", 2021, "sampleToken");
         assertEquals(vehicle.getYear(), 2021);
     }
 
+    /**
+     * Test for year setter
+     */
     public void testSetYear() {
         Vehicle vehicle = new Vehicle();
         int year = 2021;
